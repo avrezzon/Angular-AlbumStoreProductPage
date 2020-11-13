8b1f7b26d72d9b256180c29c45ae329f6ee4f39b
avrezzon <anthonyrezz@gmail.com>
2020-11-13 15:22:42 -0700

    Ok now this is just being petty

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,5 +14,5 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album>{
-    return this._http.get(this._albumUrl).map((response: Response) => <Album>response.json());
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
